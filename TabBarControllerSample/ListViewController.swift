99ee0df63809b31fb43a70d0d689144a1e0f5ab9
shima <shima11@users.noreply.github.com>
2017-01-23 02:14:14 +0900

    add ViewController(List, Map)

---
+++ TabBarControllerSample/ListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ListViewController.swift
+//  TabBarControllerSample
+//
+//  Created by shima jinsei on 2017/01/23.
+//  Copyright © 2017年 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        let swipeGesture = UISwipeGestureRecognizer()
+//        swipeGesture.direction = .left
+//        swipeGesture.numberOfTouchesRequired = 1
+//        swipeGesture.addTarget(self, action: #selector(self.swipeLeft(_:)))
+        
+//        view.addGestureRecognizer(swipeGesture)
+    }
+
+//    func swipeLeft(_ sender: UISwipeGestureRecognizer) {
+//        tabBarController?.selectedIndex = 1
+//    }
+
+}
