dd021cba148a44fb48a2a95d1b5abd6ccb9c7bed
shima <shima11@users.noreply.github.com>
2017-01-23 03:46:04 +0900

    tab icon animation

--- TabBarControllerSample/CustomTabbarController.swift
+++ TabBarControllerSample/CustomTabbarController.swift
@@ -30,38 +30,52 @@ public extension UIImage {
     }
 }
 
+class TabBar: UITabBar {
+    
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        var size = super.sizeThatFits(size)
+        size.height = 40
+        return size
+    }
+}
+
 class CustomTabbarController: UITabBarController {
     
     let addButton: UIButton = UIButton(type: .custom)
+    
+    var firstItemImageView: UIImageView!
+    var secondItemImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         delegate = self
         
-        for item in tabBar.items! {
-            if let image = item.image {
-                item.image = image.imageWithColor(tintColor: .white).withRenderingMode(.alwaysOriginal)
-                item.selectedImage = image.imageWithColor(tintColor: .lightGray).withRenderingMode(.alwaysOriginal)
-            }
-        }
+//        for item in tabBar.items! {
+//            if let image = item.image {
+//                item.image = image.imageWithColor(tintColor: .lightText).withRenderingMode(.alwaysOriginal)
+//                item.selectedImage = image.imageWithColor(tintColor: .darkGray).withRenderingMode(.alwaysOriginal)
+//            }
+//        }
+        
+        tabBar.items?[0].tag = 0
+        tabBar.items?[1].tag = 1
+        
+        let firstItemView = tabBar.subviews[0]
+        firstItemImageView = firstItemView.subviews.first as! UIImageView
+        firstItemImageView.contentMode = .center
+        
+        let secondItemView = tabBar.subviews[1]
+        secondItemImageView = secondItemView.subviews.first as! UIImageView
+        secondItemImageView.contentMode = .center
         
         UITabBar.appearance().tintColor = .darkText
-        UITabBar.appearance().backgroundColor = .white
+        UITabBar.appearance().backgroundColor = .groupTableViewBackground
         UITabBar.appearance().selectionIndicatorImage = UIImage()
         UITabBar.appearance().shadowImage = UIImage()
+        UITabBar.appearance().backgroundImage = UIImage()
         
         setButton()
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        
-        var tabFrame = tabBar.frame
-        tabFrame.size.height = 40
-        tabFrame.origin.y = view.frame.height - 40
-        tabBar.frame = tabFrame
-        
     }
     
     private func setButton() {
@@ -71,8 +85,11 @@ class CustomTabbarController: UITabBarController {
         addButton.layer.borderWidth = 4
         addButton.layer.borderColor = UIColor.white.cgColor
         addButton.clipsToBounds = true
-        addButton.setImage(UIImage(named: "add_circle"), for: UIControlState())
-        addButton.setImage(UIImage(named:"add_circle_selected"), for: UIControlState.highlighted)
+        
+        let image = UIImage(named: "add_circle")?.imageWithColor(tintColor: .darkGray).withRenderingMode(.alwaysOriginal)
+        let selectedImage = UIImage(named: "add_circle_selected")?.imageWithColor(tintColor: .darkGray).withRenderingMode(.alwaysOriginal)
+        addButton.setImage(image, for: UIControlState())
+        addButton.setImage(selectedImage, for: UIControlState.highlighted)
         addButton.addTarget(self, action: #selector(self.addButtonTapped(_:)), for: .touchUpInside)
         view.addSubview(addButton)
     }
@@ -84,6 +101,31 @@ class CustomTabbarController: UITabBarController {
 
 extension CustomTabbarController: UITabBarControllerDelegate {
     override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        
+        if item.tag == 0 {
+            let animation = CAKeyframeAnimation()
+            animation.keyPath = "transform.scale"
+            animation.duration = 0.3
+            animation.values = [NSNumber(value:0.8),
+                                NSNumber(value:1.2)
+            ]
+            animation.timingFunctions = [ CAMediaTimingFunction.init(name: kCAMediaTimingFunctionEaseInEaseOut),
+                                         CAMediaTimingFunction.init(name: kCAMediaTimingFunctionEaseInEaseOut)
+            ]
+            animation.isRemovedOnCompletion = true
+            
+            firstItemImageView.layer.add(animation, forKey: "bounce")
+            
+//            firstItemImageView.transform = CGAffineTransform.identity
+//            UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseInOut, animations: { () -> Void in
+//                let rotation = CGAffineTransform(rotationAngle: CGFloat(M_PI_2))
+//                self.firstItemImageView.transform = rotation
+//            }, completion: nil)
+        } else if item.tag == 1 {
+            secondItemImageView.transform = CGAffineTransform.identity
+            UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseInOut, animations: { () -> Void in
+                let rotation = CGAffineTransform(rotationAngle: CGFloat(M_PI_2))
+                self.secondItemImageView.transform = rotation
+            }, completion: nil)
+        }
     }
 }
