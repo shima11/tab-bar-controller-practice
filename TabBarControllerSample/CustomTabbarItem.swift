409aa86c616463006315c5a94c53d90cbc5c2866
shima <shima11@users.noreply.github.com>
2017-01-22 23:36:47 +0900

    add custom tabbaritem

---
+++ TabBarControllerSample/CustomTabbarItem.swift
@@ -0,0 +1,14 @@
+//
+//  CustomTabbarItem.swift
+//  TabBarControllerSample
+//
+//  Created by shima jinsei on 2017/01/22.
+//  Copyright © 2017年 Jinsei Shima. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomTabarItem: UIButton {
+    
+}
