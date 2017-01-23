2d16e08e4e9790aacb3388d69b302f7e5e3ca1f6
shima <shima11@users.noreply.github.com>
2017-01-23 13:05:15 +0900

    refractoring

--- TabBarControllerSample/CustomTabbarController.swift
+++ TabBarControllerSample/CustomTabbarController.swift
@@ -9,36 +9,6 @@
 import Foundation
 import UIKit
 
-public extension UIImage {
-    func imageWithColor(tintColor: UIColor) -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
-        
-        let context = UIGraphicsGetCurrentContext()! as CGContext
-        context.translateBy(x: 0, y: self.size.height)
-        context.scaleBy(x: 1.0, y: -1.0);
-        context.setBlendMode(.normal)
-        
-        let rect = CGRect(x:0, y:0, width:size.width, height:size.height)
-        context.clip(to: rect, mask: self.cgImage!)
-        tintColor.setFill()
-        context.fill(rect)
-        
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()! as UIImage
-        UIGraphicsEndImageContext()
-        
-        return newImage
-    }
-}
-
-class TabBar: UITabBar {
-    
-    override func sizeThatFits(_ size: CGSize) -> CGSize {
-        var size = super.sizeThatFits(size)
-        size.height = 40
-        return size
-    }
-}
-
 class CustomTabbarController: UITabBarController {
     
     let addButton: UIButton = UIButton(type: .custom)
@@ -69,13 +39,16 @@ class CustomTabbarController: UITabBarController {
         secondItemImageView = secondItemView.subviews.first as! UIImageView
         secondItemImageView.contentMode = .center
         
+        setTabBar()
+        setButton()
+    }
+    
+    private func setTabBar() {
         UITabBar.appearance().tintColor = .darkText
         UITabBar.appearance().backgroundColor = .groupTableViewBackground
         UITabBar.appearance().selectionIndicatorImage = UIImage()
         UITabBar.appearance().shadowImage = UIImage()
         UITabBar.appearance().backgroundImage = UIImage()
-        
-        setButton()
     }
     
     private func setButton() {
@@ -96,12 +69,15 @@ class CustomTabbarController: UITabBarController {
     
     @objc private func addButtonTapped(_ sender: UITapGestureRecognizer) {
         print("addbutton tapped")
+        
     }
 }
 
 extension CustomTabbarController: UITabBarControllerDelegate {
+    
     override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        if item.tag == 0 {
+        if item.tag == 0 { // first tab selected
+            // bounce animation
             let animation = CAKeyframeAnimation()
             animation.keyPath = "transform.scale"
             animation.duration = 0.3
@@ -112,15 +88,9 @@ extension CustomTabbarController: UITabBarControllerDelegate {
                                          CAMediaTimingFunction.init(name: kCAMediaTimingFunctionEaseInEaseOut)
             ]
             animation.isRemovedOnCompletion = true
-            
-            firstItemImageView.layer.add(animation, forKey: "bounce")
-            
-//            firstItemImageView.transform = CGAffineTransform.identity
-//            UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseInOut, animations: { () -> Void in
-//                let rotation = CGAffineTransform(rotationAngle: CGFloat(M_PI_2))
-//                self.firstItemImageView.transform = rotation
-//            }, completion: nil)
-        } else if item.tag == 1 {
+            firstItemImageView.layer.add(animation, forKey: nil)
+        } else if item.tag == 1 { // second tab selected
+            // rotate animation
             secondItemImageView.transform = CGAffineTransform.identity
             UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseInOut, animations: { () -> Void in
                 let rotation = CGAffineTransform(rotationAngle: CGFloat(M_PI_2))
